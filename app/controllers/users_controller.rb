068210218cf1121cfce93bad2719f4685cfcc69e
Mario Gamez <mariogamez@DevMm.local>
2017-12-11 16:52:19 -0600

    Session implementation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   
-  before_action :confirm_logged_in, :except => [:new,:create]
-  before_action :confirm_admin, :except => [:new,:create]
+  before_action :confirm_logged_in
+  before_action :confirm_admin
   before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   # GET /users
@@ -10,11 +10,7 @@ class UsersController < ApplicationController
     @users = User.all
   end
 
-  # GET /users/1
-  # GET /users/1.json
-  def show
-  end
-
+  
   # GET /users/new
   def new
     @user = User.new
@@ -28,29 +24,24 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
-    respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.save        
+      flash[:notice] = "User was successfully created."
+      redirect_to(users_path)
+    else
+      flash[:notice] = "Error creating user."
+      redirect_to(users_path)        
     end
   end
 
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
-      else
-        format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.update(user_params)
+      flash[:notice] = "User was successfully updated."
+      redirect_to(users_path)
+    else
+      flash[:notice] = "Error updating user."
+      redirect_to(users_path) 
     end
   end
 
