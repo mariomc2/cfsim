fd6f354b9bf39554e193e09a22be1266a807b66c
Mario Gamez <mariogamez@DevMm.local>
2017-12-10 22:21:10 -0600

    Routes root

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,9 @@ Rails.application.routes.draw do
   
 	#root :to => 'access#login'
 
-	scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
+	#scope "(:locale)", locale: /#{I18n.available_locales.join("|")}/ do
 
 		root 'projections#inputs'
-
-		resources :users, :except => [:show] do
-			member do
-				get :delete
-			end
-		end
 				
 		get 'access/menu'
 	  get 'access/login'
@@ -20,7 +14,13 @@ Rails.application.routes.draw do
 	  get 'projections/inputs'
 	  post 'projections/create'
 	  get 'projections/calculate'
-	end
+
+	  resources :users, :except => [:show] do
+			member do
+				get :delete
+			end
+		end
+	#end
   
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
